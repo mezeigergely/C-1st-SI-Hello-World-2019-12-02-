204a7a485e7c49fa3bb4664901bfe2882cf458c5
Gergely Mezei <mezei.gergely89@gmail.com>
2020-01-10 14:44:32 +0100

    semmi

---
+++ dotnet-HW-SI-ASSIGNMENT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dotnet_HW_SI_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                
+                Console.WriteLine("Hello {0}!",args[i]);
+            }
+        }
+    }
+}
